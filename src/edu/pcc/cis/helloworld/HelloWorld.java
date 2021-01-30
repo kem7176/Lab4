5a2b185e70fd174d9fcd680a9ef2ce22fb5520a6
Kiah McIntosh <kiah.mcintosh@pcc.edu>
2021-01-30 13:34:45 -0800

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,7 +1,15 @@
 package edu.pcc.cis.helloworld;
+import java.util.Date;
+
+/**
+ * Created class that prints date and message
+ * @ author Kiah McIntosh
+ * @ version 01/29/2021
+ */
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, world!");
+        System.out.print(new Date());
     }
 }
